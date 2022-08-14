e3a571c8bfd1bd7da4ce79a9330a57cf66a184d1
Piyush Diwaker <piyush@Piyushs-MacBook-Air.local>
2022-08-15 00:21:31 +0530

    some minor update, app deployed in heroku

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cmjmr!nrrq_o&5_uobgv2&7h@)8r5c^jn*wo!2l^z_-8jwcf6*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["weather-data-123.herokuapp.com","localhost"]
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
